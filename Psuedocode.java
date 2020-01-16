13be8d46b0523241a982f56f0d4fc996753faddf
Kennashka DeSilva <mail@kennashka.com>
2020-01-16 10:08:28 -0500

    Create Psuedocode.java

---
+++ Psuedocode.java
@@ -0,0 +1,34 @@
+// Make breakfast Psuedocode (How to make 2 eggs, 2 veggie bacon, and 1 slice of bread)
+
+/*
+1. Look how many eggs are in fridge
+2. var eggs = number of eggs
+3. Look how many bacon strips are in fridge
+4. var bacon = number of bacon strips
+5. Look how many slices of bread are available
+6. var bread = number of bread slices
+7. make sure eggs >= 2, bacon >= 2, bread >=1
+8. Get a non-stick frying pan
+9. Get a spatula 
+10. Get a plate
+11. Get a fork and knife
+12. var cookTime = 4 
+13. Find stove 
+14. Put frying pan on stove
+15. Turn on stove, put on medium high
+16. Crack 2 eggs onto frying pan
+17. Let eggs cook for cookTime minutes 
+18. Put eggs on plate with spatula
+19. Put 2 bacon strips on frying pan
+20. cookTime = 5
+21. Let bacon cook for cookTime minutes
+22. Put bacon on plate with spatula 
+23. Find and plug in toaster
+24. Put 1 bread slice in toaster
+25. cookTime = 1
+26. Set timer for cookTime minutes
+27. Put toasted bread on plate
+28. Take plate, fork and knife to table
+29. Tell the user "Enjoy"
+
+*/
