2ac6b41b719b356fb2bb1bec43280c096c47b9ff
Kennashka DeSilva <mail@kennashka.com>
2020-02-04 09:00:03 -0500

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,21 @@
+class hello_world {
+  public static void main(String[] args) {
+    System.out.println("Hello world! 🌍");
+
+
+    String firstName = "Kennashka";
+
+     String middleName = "hack";
+
+      String lastName = "DeSilva";
+/*
+    2. name and initials
+Write a program that has the following String variables: firstName, middleName, and lastName. Initialize these with your first, middle, and last names. The program should also have the following char variables: firstInitial, middleInitial, and lastInitial. Store your first, middle, and last initials in these variables. The program should display the con- tents of these variables on the screen.
+
+*/
+
+
+
+System.out.println("\n" + firstName + " " +  middleName + " " + lastName);
+  }
+}
