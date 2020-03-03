6afa64c3a1248d0123ad17a355e9ea5c46b2cf92
Kennashka DeSilva <mail@kennashka.com>
2020-03-03 11:25:25 -0500

    Create clock.java

---
+++ clock.java
@@ -0,0 +1,13 @@
+//        
+//   for (int hours = 1; hours <= 12; hours++)      {         
+//
+//        for (int minutes = 0; minutes <= 59; minutes++)          {                            
+//
+//                          System.out.println(hours + ":" +  minutes );     
+//                          
+//        for (int seconds = 0; seconds <= 59; seconds++)          {                            
+//
+//             System.out.println(hours + ":" +  minutes + ":" +  seconds  );     
+//        }
+//     }       
+//  }
